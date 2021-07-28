0a828c987aebcaa0969d3a1ecee17f4d711877f6
dhowai <dhowai.dev@gmail.com>
2021-07-28 18:21:35 +0000

    Add basic board layout for game

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-# python code goes here
+print("             Battleships        \n")
+print("     0  1  2  3  4  5  6  7  8  9")
+
+for x in range(10):
+    row = ""
+    for y in range(10):
+        ch = " _ "
+        row = row + ch
+    print(x, " ", row)
